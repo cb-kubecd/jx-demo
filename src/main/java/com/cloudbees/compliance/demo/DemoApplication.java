72fa4ffa10e232535996e8c924d491b41ff3dc10
Dominic Gunn <DominicGunn@users.noreply.github.com>
2018-10-31 16:40:10 +0000

    Update DemoApplication.java

--- src/main/java/com/cloudbees/compliance/demo/DemoApplication.java
+++ src/main/java/com/cloudbees/compliance/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test pr");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
