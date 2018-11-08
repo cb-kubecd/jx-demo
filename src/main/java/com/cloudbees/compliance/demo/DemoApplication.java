cd6585e0e08eb0ef668d07cf9592f7ea7a25fdc1
Pete Muir <pmuir@bleepbleep.org.uk>
2018-11-08 14:32:51 +0000

    feat: add 3 static analysis issues

--- src/main/java/com/cloudbees/compliance/demo/DemoApplication.java
+++ src/main/java/com/cloudbees/compliance/demo/DemoApplication.java
@@ -10,4 +10,23 @@ public class DemoApplication {
 		System.out.println("Test pr");
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	public Boolean booleanMethod() {
+		return null;
+	}
+
+	public static double circumference(double diameter) {
+		return diameter * 3.141;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		DemoApplication toCompare = (DemoApplication) obj;
+		return toCompare == this;
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 }
