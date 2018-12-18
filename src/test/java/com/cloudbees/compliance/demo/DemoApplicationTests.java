4bc6ba661c0fb87b1968ae4d1530bc4159c16eee
Keith Zantow <kzantow@gmail.com>
2018-12-18 07:38:56 -0500

    change code coverage

--- src/test/java/com/cloudbees/compliance/demo/DemoApplicationTests.java
+++ src/test/java/com/cloudbees/compliance/demo/DemoApplicationTests.java
@@ -5,10 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void circumference() {
+		assert DemoApplication.circumference(1) > 0;
 	}
 
 }
